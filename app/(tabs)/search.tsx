8ba59de11723493282ab9d93a0e3e6e589694e7f
Arqum Malik <arqummalik1@gmail.com>
2025-04-22 23:20:19 +0530

    initial commit

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,102 @@
+import { View, Text, Image, FlatList, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+import { images } from "@/constants/images";
+import MovieCard from "@/components/movieCard";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+
+const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const {
+    data: movies,
+    loading,
+    error,
+    refetch,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  useEffect(() => {
+    const debounceFunc = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await refetch();
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => clearTimeout(debounceFunc);
+  }, [searchQuery]);
+
+  return (
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w0full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "flex-start",
+          paddingRight: 5,
+          marginBottom: 10,
+        }}
+        className="mt-2 pb-32"
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search movies"
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+              />
+
+              {loading && (
+                <ActivityIndicator
+                  size="large"
+                  color="#0000ff"
+                  className="my-3"
+                />
+              )}
+              {error && (
+                <Text className="text-red-500 px-5 my-3">
+                  Error : {error.message}
+                </Text>
+              )}
+
+              {!loading &&
+                !error &&
+                searchQuery.trim() &&
+                movies?.length > 0 && (
+                  <Text className="text-xl text-white font-bold">
+                    Search Result for :
+                    <Text className="text-accent">{searchQuery}</Text>
+                  </Text>
+                )}
+            </View>
+          </>
+        }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text className='text-center text-gray-500'>
+                {searchQuery.trim() ? 'No Movies Found ' : "Search for Movies"}
+                </Text>
+            </View>
+          ) : null
+        }
+      />
+    </View>
+  );
+};
+
+export default Search;
---
+++ services/useFetch.ts
@@ -0,0 +1,44 @@
+// useFetch
+//fetchMovies
+//fetchMovieDetails
+
+import { useEffect, useState } from "react"
+
+const useFetch = <T>(fetchFunction : ()=> Promise<T>, autoFetch = true)=> {
+    const [data,setData] = useState<T | null>(null);
+    const [loading,setLoading] = useState(false);
+    const [error,setError] = useState<Error | null>(null)
+
+    const fetchData = async ()=> {
+        try {
+            setLoading(true)
+            setError(null)
+
+            const result = await fetchFunction();
+            setData(result)
+
+        }
+        catch (err) {
+            setError(err instanceof Error ? err : new Error("An Error Occured in useFetch"))
+        }
+        finally{
+            setLoading(false)
+        }
+    };
+
+    const reset = ()=> {
+        setData(null);
+        setLoading(false);
+        setError(null)
+    };
+
+    useEffect(()=> {
+        if(autoFetch){
+            fetchData();
+        }
+    },[]);
+
+    return {data, error, loading, reset, refetch : fetchData };
+}
+
+export default useFetch;
